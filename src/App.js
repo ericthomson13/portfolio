3be1471e128ca7fd1979c48de6c2849cfd948c4b
et <eric.thomson13@gmail.com>
2020-05-13 20:58:58 -0600

    eslint Configured

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App" >
-    <div className="w-full max-w-md bg-gray-800" >
-      <div>This is my new website!</div>
-    </div>
+const App = () => (
+  <div className="App" >
+    <div>This is my new website!</div>
   </div>
-  );
-}
+);
 
 export default App;
