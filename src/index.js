df3a93bd561bec96a52357215e0f98addc327044
eric.thomson <eric.thomson13@gmail.com>
2026-03-14 20:04:31 -0600

    Tailwind to SCSS migration

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './styles/tailwind.css';
+import './styles/index.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
--- tailwind.legacy.js
+++
@@ -1,2 +0,0 @@
-// Legacy Tailwind config preserved from tailwind.js (pre-migration backup)
-module.exports = require('./tailwind.js');
